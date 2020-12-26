d14f8a2b9f5bd8ab33c477d865b801eb32f7588f
mishkat81 <mishkatnipro@gmail.com>
2020-12-26 21:24:39 +0600

    Added working duration
    Here i add working duration using javascript for dynamically.

--- main.js
+++ main.js
@@ -1,13 +1,66 @@
+//working duration start
+ 
 
+function calculate_age(x){
+    var birth_date = new Date(x);
+    var birth_date_day = birth_date.getDate();
+    var birth_date_month = birth_date.getMonth();
+    var birth_date_year = birth_date.getFullYear();
+
+    var toDay_date = new Date();
+    var toDay_date_day = toDay_date.getDate();
+    var toDay_date_month = toDay_date.getMonth();
+    var toDay_date_year = toDay_date.getFullYear();
+
+    var calculated_age = 0;
+    var calculated_month = 0;
+    var calculated_day = 0; 
+    var calculated_age = toDay_date_year - birth_date_year;
+    
+
+    if(toDay_date_day > birth_date_day){
+        calculated_day = toDay_date_day - birth_date_day;
+        calculated_age = toDay_date_year - birth_date_year;
+        calculated_month = toDay_date_month - birth_date_month;
+   }else if(toDay_date_day < birth_date_day){
+        calculated_day = (toDay_date_day + 30) - birth_date_day;
+        calculated_month = toDay_date_month - birth_date_month - 1;
+        calculated_age = toDay_date_year - birth_date_year - 1;
+   }else if(toDay_date_month > birth_date_month){
+        calculated_age = toDay_date_year - birth_date_year;
+        calculated_month = toDay_date_month - birth_date_month;
+   }else if(toDay_date_month < birth_date_month){
+        calculated_age = toDay_date_year - birth_date_year - 1;
+        calculated_month = (toDay_date_month + 12) - birth_date_month;
+   }else{
+    calculated_age = toDay_date_year - birth_date_year;
+   }
+
+    return ('Year: '+calculated_age+' Month: '+calculated_month+' Day: '+calculated_day);
+
+}
+var kamrul = calculate_age("02/26/2019");
+var ibrahim = calculate_age("01/14/2012");
+var faysal = calculate_age("03/19/2018");
+var shohel = calculate_age("03/15/2016");
+var haron = calculate_age("02/26/2019");
+var mishkat = calculate_age("03/16/2018");
+
+//working duration end
+
+
+
+//Colleagues information using object
 const colleguesDetails = [
-    {'name':'Kamrul Islam','designation':'medical information officer', 'id':'04320', 'dateOfJoin':'26 Feb 2019', 'workingDuration':'Year:02 Month:04 Day:17','pCompany':'Delta Pharmaceutical', 'address':'Feni district', 'mobile':'01713656339/01811296879','district':'feni','email':'kamrul@gmail.com'},
-    {'name':'Ebrahim Khalil', 'designation':' Senior medical information officer', 'id':'00305', 'dateOfJoin':'14 Jan 2012', 'workingDuration':'Year:02 Month:04 Day:17','pCompany':'---- Pharmaceutical', 'address':'Feni district', 'mobile':'01777779443/01818377997','district':'feni','email':'ibrahim@gmail.com'},
-    {'name':'Faysal karim', 'designation':'medical information officer', 'id':'03836', 'dateOfJoin':'19 Mar 2018', 'workingDuration':'Year:02 Month:04 Day:17','pCompany':'Sharif Pharmaceutical', 'address':'Feni district', 'mobile':'01713656117/01644219887','district':'feni','email':'faysal2964@gmail.com'},
-    {'name':'Shohel Rana', 'designation':' Senior medical information officer', 'id':'02547', 'dateOfJoin':'15 Mar 2016', 'workingDuration':'Year:02 Month:04 Day:17','pCompany':'ACI Pharmaceutical', 'address':'Rangpur district', 'mobile':'01777780066/01725986282','district':'Rangpur','email':'sohel@gmail.com'},
-    {'name':'Harun or rashid', 'designation':'medical information officer', 'id':'04319', 'dateOfJoin':'26 Feb 2019', 'workingDuration':'Year:02 Month:04 Day:17','pCompany':'Kemico Pharmaceutical', 'address':'Noakhali district', 'mobile':'01713656338/01836683860','district':'Noakhali','email':'haron@gmail.com'},
-    {'name':'Mishkat Ahmed', 'designation':'Area Manager', 'id':'03711', 'dateOfJoin':'16 Mar 2018', 'workingDuration':'Year:02 Month:04 Day:17','pCompany':'Aristo Pharma Ltd', 'address':'Moulvi bazar district', 'mobile':'01777779546','district':'Moulvi Bazar','email':'mishkatnipro@gmail.com'},
+    {'name':'Kamrul Islam','designation':'medical information officer', 'id':'04320', 'dateOfJoin':'26 Feb 2019', 'workingDuration':`${kamrul}`,'pCompany':'Delta Pharmaceutical', 'address':'Feni district', 'mobile':'01713656339/ 01811296879','district':'feni','email':'kamrul@gmail.com'},
+    {'name':'Ebrahim Khalil', 'designation':' Senior medical information officer', 'id':'00305', 'dateOfJoin':'14 Jan 2012', 'workingDuration':`${ibrahim}`,'pCompany':'---- Pharmaceutical', 'address':'Feni district', 'mobile':'01777779443/ 01818377997','district':'feni','email':'ibrahim@gmail.com'},
+    {'name':'Faysal karim', 'designation':'medical information officer', 'id':'03836', 'dateOfJoin':'19 Mar 2018', 'workingDuration':`${faysal}`,'pCompany':'Sharif Pharmaceutical', 'address':'Feni district', 'mobile':'01713656117/ 01644219887','district':'feni','email':'faysal2964@gmail.com'},
+    {'name':'Shohel Rana', 'designation':' Senior medical information officer', 'id':'02547', 'dateOfJoin':'15 Mar 2016', 'workingDuration':`${shohel}`,'pCompany':'ACI Pharmaceutical', 'address':'Rangpur district', 'mobile':'01777780066/ 01725986282','district':'Rangpur','email':'sohel@gmail.com'},
+    {'name':'Harun or rashid', 'designation':'medical information officer', 'id':'04319', 'dateOfJoin':'26 Feb 2019', 'workingDuration':`${haron}`,'pCompany':'Kemico Pharmaceutical', 'address':'Noakhali district', 'mobile':'01713656338/ 01836683860','district':'Noakhali','email':'haron@gmail.com'},
+    {'name':'Mishkat Ahmed', 'designation':'Area Manager', 'id':'03711', 'dateOfJoin':'16 Mar 2018', 'workingDuration':`${mishkat}`,'pCompany':'Aristo Pharma Ltd', 'address':'Moulvi bazar district', 'mobile':'01777779546','district':'Moulvi Bazar','email':'mishkatnipro@gmail.com'},
 ];
 
+//create a function for displaying object items into a table for dynamically
 function myAction(data){
 
     let table = document.getElementById('collegues-details');

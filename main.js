3926afb4f82258fc41ba8a383bb16327b21da24b
mishkat81 <mishkatnipro@gmail.com>
2020-12-28 15:28:53 +0600

    Correction Faysal DOJ
    Faysal date of join correction

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ function calculate_age(x){
 }
 var kamrul = calculate_age("02/26/2019");
 var ibrahim = calculate_age("01/14/2012");
-var faysal = calculate_age("03/19/2018");
+var faysal = calculate_age("05/19/2018");
 var shohel = calculate_age("03/15/2016");
 var haron = calculate_age("02/26/2019");
 var mishkat = calculate_age("03/16/2018");
@@ -54,7 +54,7 @@ var mishkat = calculate_age("03/16/2018");
 const colleguesDetails = [
     {'name':'Kamrul Islam','designation':'medical information officer', 'id':'04320', 'dateOfJoin':'26 Feb 2019', 'workingDuration':`${kamrul}`,'pCompany':'Delta Pharmaceutical', 'address':'House:338 Hazi omar ali bary, Vill: Middle Charipur, PO:Feni Sadar-3900, PS:feni sadar, Dist: Feni.', 'mobile':'01713656339/ 01811296879','district':'feni','email':'----@gmail.com'},
     {'name':'Ebrahim Khalil', 'designation':' Senior medical information officer', 'id':'00305', 'dateOfJoin':'14 Jan 2012', 'workingDuration':`${ibrahim}`,'pCompany':'---- Pharmaceutical', 'address':'Vill:Maolana shaheber bari, South manikpur, PO:Manikpur-3860, PS:Senbag, Dist:Noakhali.', 'mobile':'01777779443/ 01818377997','district':'Noakhali','email':'-----@gmail.com'},
-    {'name':'Faysal karim', 'designation':'medical information officer', 'id':'03836', 'dateOfJoin':'19 Mar 2018', 'workingDuration':`${faysal}`,'pCompany':'Sharif Pharmaceutical', 'address':'Vill:Bazlur Rahman Master bari, East Kashipur, PO:Kashipur, PS:Chagolnaiya, Dist:Feni.', 'mobile':'01713656117/ 01644219887','district':'feni','email':'faysal2964@gmail.com'},
+    {'name':'Faysal karim', 'designation':'medical information officer', 'id':'03836', 'dateOfJoin':'19 May 2018', 'workingDuration':`${faysal}`,'pCompany':'Sharif Pharmaceutical', 'address':'Vill:Bazlur Rahman Master bari, East Kashipur, PO:Kashipur, PS:Chagolnaiya, Dist:Feni.', 'mobile':'01713656117/ 01644219887','district':'feni','email':'faysal2964@gmail.com'},
     {'name':'Shohel Rana', 'designation':' Senior medical information officer', 'id':'02547', 'dateOfJoin':'15 Mar 2016', 'workingDuration':`${shohel}`,'pCompany':'ACI Pharmaceutical', 'address':'Vill:Rajman Dohopara,Rajman, PO:Rajman-6762, PS:Ullapara, Dist:Sirajganj.', 'mobile':'01777780066/ 01725986282','district':'Sirajganj','email':'----@gmail.com'},
     {'name':'Harun or rashid', 'designation':'medical information officer', 'id':'04319', 'dateOfJoin':'26 Feb 2019', 'workingDuration':`${haron}`,'pCompany':'Kemico Pharmaceutical', 'address':'Noakhali district', 'mobile':'01713656338/ 01836683860','district':'Noakhali','email':'-----@gmail.com'},
     {'name':'Mishkat Ahmed', 'designation':'Area Manager', 'id':'03711', 'dateOfJoin':'16 Mar 2018', 'workingDuration':`${mishkat}`,'pCompany':'Aristo Pharma Ltd', 'address':'Vill:Sankarpur, PO:Dasher Bazar, PS:Baralekha, Dist:Moulvi Bazar', 'mobile':'01777779546','district':'Moulvi Bazar','email':'mishkatnipro@gmail.com'},
